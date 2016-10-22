a97f73bd3e45feea644fc3e0933f2d317a797b80
Gilberto Ademar Freitas Filho <gilberto.ademar@gmail.com>
2016-10-22 18:54:07 -0200

    Começando o projeto Aps 8 semestre

---
+++ src/main/webapp/javaScript/scriptSistema/aps.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+//	$.material.init();
+
+	$(function() {
+		$(".btn-toggle").click(function(e) {
+			e.preventDefault();
+			el = $(this).data('element');
+			$(el).toggle();
+		});
+	});
+
+	function deixarDivsInvisiveis() {
+		$('#idHome').toggle();
+		$('#idPqEconomizar').toggle();
+		$('#idComoEcon').toggle();
+		$('#idCalculeConsumo').toggle();
+
+	}
+
+	$('#btnHome').click(function() {
+		$('#idHome').show();
+		$('#idPqEconomizar').hide();
+		$('#idComoEcon').hide();
+		$('#idCalculeConsumo').hide();
+	});
+
+	$('#btnPqEconomizar').click(function() {
+		$('#idHome').hide();
+		$('#idPqEconomizar').show();
+		$('#idComoEcon').hide();
+		$('#idCalculeConsumo').hide();
+	});
+
+	$('#btnComoEcon').click(function() {
+		$('#idHome').hide();
+		$('#idPqEconomizar').hide();
+		$('#idComoEcon').show();
+		$('#idCalculeConsumo').hide();
+	});
+
+	$('#btnCalcule').click(function() {
+		$('#idHome').hide();
+		$('#idPqEconomizar').hide();
+		$('#idComoEcon').hide();
+		$('#idCalculeConsumo').show();
+	});
+
+	deixarDivsInvisiveis();
+});
